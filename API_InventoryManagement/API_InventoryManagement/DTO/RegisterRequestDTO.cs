b3d9fe5709425ce8daebbcd9d67ea616dd8ed399
Huy Nguyen <huuhuy5208@gmail.com>
2023-08-22 18:19:09 +0700

    create login, register, logout form and index page for client

---
+++ API_InventoryManagement/API_InventoryManagement/DTO/RegisterRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_InventoryManagement.DTO
+{
+    public class RegisterRequestDTO
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string FullName { get; set; }
+    }
+}
---
+++ Client_InventoryManagement/Client_InventoryManagement/DTO/RegisterRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Client_InventoryManagement.DTO
+{
+    public class RegisterRequestDTO
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string FullName { get; set; }
+    }
+}
